0c41d7e890409b2c01a2ed63ce07e9ff96c17b39
gcrowder <gregory.j.crowder@gmail.com>
2016-04-06 18:16:43 -0400

    Finished normal mode by implementing and testing all vector functions. Three matrix functions are also implemented and tested. Well, two matrix functions plus shape.

--- linear_algebra.py
+++ linear_algebra.py
@@ -1,3 +1,4 @@
+import math
 class ShapeError(Exception):
     pass
 
@@ -12,7 +13,48 @@ def shape(vector_matrix):
 
 def vector_add(vector_one, vector_two):
     if shape(vector_one) == shape(vector_two):
-        added_vectors = [a+b for a, b, in zip(vector_one, vector_two)]
+        added_vectors = [a + b for a, b in zip(vector_one, vector_two)]
     else:
         raise ShapeError("Cannot add vectors of different dimensions")
     return added_vectors
+
+def vector_sub(vector_one, vector_two):
+    if shape(vector_one) == shape(vector_two):
+        subtracted_vectors = [a - b for a, b in zip(vector_one, vector_two)]
+    else:
+        raise ShapeError("Cannot subtract vectors of different dimensions")
+    return subtracted_vectors
+
+def vector_sum(*args):
+    shape_results = [shape(vector) for vector in args] #put the number of rows of each vector into a list
+    if max(shape_results) == min(shape_results): #count the number of vectors that have the same amount of rows. True if equal to number of vectors.
+        #list_of_tuples = zip(vector_list)
+        summed_vectors = [sum(a) for a in zip(*args)]
+    else:
+        raise ShapeError("Cannot sum vectors of different dimensions")
+    return summed_vectors
+
+def dot(vector_one, vector_two):
+    if shape(vector_one) == shape(vector_two):
+        dot_product = sum(a*b for a, b in zip(vector_one, vector_two))
+    else:
+        raise ShapeError("Cannot multiply vectors of different dimensions")
+    return dot_product
+
+def vector_multiply(vector, scalar):
+    return [number * scalar for number in vector]
+
+def mean(*args):
+    return sum(*args) / len(*args)
+
+def vector_mean(*args):
+    return [mean(a) for a in zip(*args)]
+
+def magnitude(vector):
+    return math.sqrt((sum([number**2 for number in vector])))
+
+def matrix_row(matrix, row):
+    return matrix[row]
+
+def matrix_col(matrix, col):
+    return [row[col] for row in matrix]
